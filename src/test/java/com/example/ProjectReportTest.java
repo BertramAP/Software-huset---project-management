a268ac6f9836f83b04e4db5a8b9d778c7d1ff9d4
BertramAP <bedeampo@gmail.com>
2026-03-23 14:02:59 +0100

    Employee basic test

--- src/test/java/com/example/ProjectReportTest.java
+++ src/test/java/com/example/ProjectReportTest.java
@@ -7,20 +7,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class EmployeeTest {
-    @Given("there is an employee with initials {string}")
-    public void there_is_an_employee_with_initials(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-        Employee employee = new Employee(string);
+public class ProjectReportTest {
+    @Given("there is a project with name {string}")
+    public void there_is_a_project_with_name(String name) {
+        throw new PendingException("idfk");
     }
-    @When("the employee creates a project with name {string}")
-    public void the_employee_creates_a_project_with_name(String string) {
 
-    }
-    @Then("the project is created")
-    public void the_project_is_created() {
-    // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    @And("the employee {string} is the project leader of the project") {
+        throw new PendingException()
     }
 }
