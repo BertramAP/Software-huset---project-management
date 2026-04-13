6902daed870a4b11f14e49e348545d66263bbb7f
DiversenSato <diversensato@gmail.com>
2026-04-13 16:12:18 +0200

    feat(cli): add CreateUserCommand

---
+++ src/main/java/com/example/cli/commands/CreateUserCommand.java
@@ -0,0 +1,30 @@
+package com.example.cli.commands;
+
+import com.example.Employee;
+import com.example.Project;
+import com.example.ProjectApp;
+import com.example.cli.AbstractCommand;
+
+public class CreateUserCommand extends AbstractCommand {
+
+    public CreateUserCommand(ProjectApp app) {
+        super(app);
+    }
+
+    @Override
+    public String getUsage() {
+        return "create-user <name>";
+    }
+
+    @Override
+    public boolean onCommand(String[] args) {
+        if (!args[0].equals("create-user"))
+            return false;
+        if (args.length < 2)
+            throw new IllegalArgumentException("Usage: " + getUsage());
+
+        Employee user = app.createUser(args[1]);
+        System.out.println("Created user " + args[1] + " with ID " + user.getID());
+        return true;
+    }
+}
