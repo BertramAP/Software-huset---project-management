6a77881a5ef9d88bfeb52f0e76c12e66572b8cae
oggefaderen123 <s244771@dtu.dk>
2026-04-13 10:41:41 +0200

    Create PersonalActivity.java

---
+++ src/main/java/com/example/PersonalActivity.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Calendar;
+
+public class PersonalActivity {
+    private String name;
+    private Calendar startDate;
+    private Calendar endDate;
+
+    public PersonalActivity(String name, Calendar startDate, Calendar endDate) {
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public Calendar getEndDate() {
+        return endDate;
+    }
+
+    public void setStartDate(Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Calendar endDate) {
+        this.endDate = endDate;
+    }
+}
