84d608bb14a52ece58f55c87beeafceb315e7bc7
DiversenSato <diversensato@gmail.com>
2026-05-11 22:11:07 +0200

    inkluder flere tests i ProjectApp::createProject

--- src/test/java/WhiteBox/ProjectAppTest.java
+++ src/test/java/WhiteBox/ProjectAppTest.java
@@ -11,17 +11,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProjectAppTest {
 
     @Test
-    void createProject() {
+    void GivenManyNames_WhenCreatingProject_ShouldIncrementIds() {
         ProjectApp app = new ProjectApp();
         int year = (LocalDate.now().getYear() - 2000) * 1000;
 
         Project project1 = app.createProject("myProject");
         assertEquals(year + 1, project1.getID());
 
-        Project project2 = app.createProject("myProject");
+        Project project2 = app.createProject("test");
         assertEquals(year + 2, project2.getID());
 
-        Project project3 = app.createProject("myProject");
+        Project project3 = app.createProject("cli");
         assertEquals(year + 3, project3.getID());
+    }
+
+    @Test
+    void givenBlankName_WhenCreatingProject_ThenShouldReturnProject() {
+        ProjectApp app = new ProjectApp();
+        Project project = app.createProject("");
+        assertNotNull(project);
+    }
+
+    @Test
+    void giveNonUniqueNames_WhenCreatingProject_ThenShouldGiveUniqueIds() {
+        ProjectApp app = new ProjectApp();
+
+        String projectName = "myProject";
+        Project project1 = app.createProject(projectName);
+        Project project2 = app.createProject(projectName);
+
+        assertNotEquals(project1.getID(), project2.getID());
     }
 }
