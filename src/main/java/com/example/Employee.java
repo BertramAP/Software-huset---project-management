57c808c7738a8832319320baa98ad3eaefb11706
BertramAP <bedeampo@gmail.com>
2026-05-11 17:04:22 +0200

    isavailable fix

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -90,13 +90,14 @@ public class Employee {
         return true;
     }
     public boolean isAvailable(LocalDate start, LocalDate end) { // Wriiten by BAP
+
         for (PersonalActivity a : personalActivities) {
             LocalDate busyStart = a.getStartDate();
             LocalDate busyEnd = a.getEndDate();
             if(busyStart.isAfter(end) && busyStart.isAfter(start)) return true;
             if(start.isAfter(busyEnd) && end.isAfter(busyEnd)) return true;
         }
-        return false;
+        return personalActivities.isEmpty();
     }
     public Project getProject(int id) { // Written by BAP
         for (Project p : projects) {
